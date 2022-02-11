b05f9829f2e10b83e6468963ee80eca0463dfcb6
rahalejr <78522633+rahalejr@users.noreply.github.com>
2022-02-11 01:17:55 -0800

    Update day_4.py

--- smoothstack/assignments/day_4.py
+++ smoothstack/assignments/day_4.py
@@ -51,3 +51,33 @@ two_string = lambda x,y: x[0] == y[0]
 
 #13
 one_four = lambda x: ''.join([x[i].upper() if i == 0 or i == 3 else x[i] for i in range(len(x))])
+
+
+
+
+### Assignment 4-2
+
+
+## 1 & 2
+# I am assuming each element of the list of lists is a row from the table
+
+order_calc = lambda orders: [(i[0], i[2]*i[3]) if i[2]*i[3] >= 100 else (i[0], i[2]*(i[3]+10)) for i in orders]
+
+
+## 3
+
+def new_order(orders):
+    output = []
+    for i in orders:
+        summed = 0
+        for j in i[1:]:
+            if j[1]*j[2] >= 100:
+                summed += j[1]*j[2]
+            else:
+                summed += j[1]*(j[2]+10)
+        output += [(i[0],summed)]
+    return output
+
+test_input = [[234,(345,4,15),(654,6,20)], [345,(838,3,29),(454,4,8)]]
+
+print(new_order(test_input)) # output: [(234, 220), (345, 189)]
